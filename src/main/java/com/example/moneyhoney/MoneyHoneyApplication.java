1bee07314b69a71a73149ce7ece518fb8857e872
oguzhan.yazici <oguzhan.yazici@enuygun.com>
2024-01-20 19:17:36 +0300

    first commit

---
+++ src/main/java/com/example/moneyhoney/MoneyHoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.moneyhoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyHoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyHoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moneyhoney/MoneyHoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneyhoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyHoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
